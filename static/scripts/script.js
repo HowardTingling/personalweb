bc1995ba0496bc267b6b77919fa954faf20b5f98
Howard Ting <cuberuru@gmail.com>
2019-08-20 14:00:23 +0200

    drag-scroll on nav; not smooth

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,76 @@
 import {setHoverAnimation, setClickAnimation, coordinateList, URLify, jQueryFlags} from "/static/scripts/auxfns.js/";
+let dragReady = false;
+let navUp = true;
+let position = -565;
+let prevPos = 0;
 
+const initWindowEvents = () => {
+	let flexcontainer = document.querySelector(".flex-container");
+	window.addEventListener("mouseup", function() {
+		dragReady = false;
+	});
+	window.addEventListener("mousemove", function(event) {
+		if (dragReady == true) {
+			let currPos = event.clientY;
+			let move = currPos - prevPos;
+			if (move < 5) {
+				position = -565;
+			}
+			if (move > -5) {
+				position = 0;
+			}
+			move = 0;
+			if (position > 0) {		
+				position = 0;
+			} 
+			if (position < -565) {
+				position = -565;
+			}
+			flexcontainer.style.transform = "translateY(" + String(position) + "px)";
+			prevPos = event.clientY;
+		}
+
+	});
+}
+
+const navClick = (obj) => {
+	obj.addEventListener("click", function() {
+		if (navUp == true) {
+			$(".flex-container").css("transform", "translateY(0px)");
+		} else {
+			$(".flex-container").css("transform", "translateY(-565px)");
+		}
+		navUp = !navUp;
+	});
+}
+
+const navDrag = (obj) => {
+	let flexcontainer = document.querySelector(".flex-container");
+	let jflexcontainer = $(".flex-container");
+	obj.setAttribute("draggable", "true");
+	jQuery(obj).on("dragstart", function() {
+		return false;
+	});
+	obj.addEventListener("mousedown", function(event) {
+		dragReady = true;
+	});
+	obj.addEventListener("click", function(event) {
+		if (navUp == true) {
+			jflexcontainer.css("transform", "translateY(0%)");
+			position = 0;
+		} else {
+			jflexcontainer.css("transform", "translateY(-90%)");
+			position = -90;
+		}
+		navUp = !navUp;
+	});
+}
 const stopVideo = () => {
-	let videos = document.querySelector(".music-video");
-	videos.currentTime = 0;
-	videos.pause();
+	let videos = document.querySelectorAll(".music-video");
+	for (let i = 0; i < videos.length; i++) {
+		videos.item(i).currentTime = 0;
+		videos.item(i).pause();
+	}
 }
 
 const hideAll = () => {
@@ -29,6 +96,8 @@ const navInit = () => {
 	let bubbleElems = $(".flex-bubble");
 	let textElems = $(".flex-text");
 	let pageElems = $(".page-contents");
+	let flexcontainer = document.getElementsByClassName("flex-container")[0];
+	let jflexcontainer = $(".flex-container");
 	let bgList = [];
 	bgList.push("static/images/book_bg0.jpg");
 	bgList.push("static/images/piano_bg0.jpg");
@@ -43,8 +112,14 @@ const navInit = () => {
 			"opacity": 0
 		};
 		let toAnimation = {
-			"opacity": 1
+ 				"opacity": 1
 		};
+		touchedElem.on("dragstart", function() {
+			return false;
+		});
+		affectedElem.on("dragstart", function() {
+			return false;
+		});
 		setHoverAnimation(touchedElem, touchedElem, toAnimation, fromAnimation, 400);
 		setHoverAnimation(touchedElem, affectedElem, {"opacity":0.2}, {"opacity":1, "color":"yellow"}, 100);
 		setHoverAnimation(affectedElem, affectedElem, {"opacity":0.2}, {"opacity":1, "color":"yellow"}, 100);
@@ -61,12 +136,22 @@ const navInit = () => {
 	$("#music").on("click", function() {
 		displayPage("#music-descr");
 	});
-	setClickAnimation($("#explore"), $(".flex-container"), {"transform": "translateY(0%)"}, {"transform": "translateY(-90%)"}, 400);
-	setClickAnimation($("#explore-text"), $(".flex-container"), {"transform": "translateY(0%)"}, {"transform": "translateY(-90%)"}, 400);
 	setHoverAnimation($("#explore"), $("#explore"), {"opacity": 0.2}, {"opacity":1}, 300);
+	navDrag(document.getElementById("music"));
+	navDrag(document.getElementById("explore"));
+	navDrag(document.getElementById("portfolio"));
+	navDrag(document.getElementById("about"));
+	navDrag(document.getElementById("writing"));
+	navDrag(document.getElementById("music-text"));
+	navDrag(document.getElementById("port-text"));
+	navDrag(document.getElementById("writing-text"));
+	navDrag(document.getElementById("about-text"));
+	navDrag(flexcontainer);
+	navClick(document.getElementById("explore"));
 }
 
 $(document).ready(function(){
+	initWindowEvents();
 	pagesInit();
 	navInit();
 });
